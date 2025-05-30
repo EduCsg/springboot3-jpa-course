fee239e159cfb2bed691ca6ab89fb32438593fd5
Eduardo <eduardocasagrande71@gmail.com>
2025-05-30 19:34:54 -0300

    Feat: h2 db, test profile, jpa

--- src/main/java/com/du/javaCourse/entities/User.java
+++ src/main/java/com/du/javaCourse/entities/User.java
@@ -1,11 +1,18 @@
 package com.du.javaCourse.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
