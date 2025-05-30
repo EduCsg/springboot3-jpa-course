c97876e0faacaf8e1d1e3fda17902ac6a8b12fd0
Eduardo <eduardocasagrande71@gmail.com>
2025-05-30 20:40:43 -0300

    Feat: add OrderStatus enum

--- src/main/java/com/du/javaCourse/config/TestConfig.java
+++ src/main/java/com/du/javaCourse/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.du.javaCourse.config;
 
 import com.du.javaCourse.entities.Order;
 import com.du.javaCourse.entities.User;
+import com.du.javaCourse.entities.enums.OrderStatus;
 import com.du.javaCourse.repositories.OrderRepository;
 import com.du.javaCourse.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Teste01", "teste1@gmail.com", "99999999", "123456");
         User u2 = new User(null, "Teste02", "teste2@gmail.com", "88888888", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(List.of(u1, u2));
         orderRepository.saveAll(List.of(o1, o2, o3));
