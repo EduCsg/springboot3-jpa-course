09ba64c1ac2edf4cbd8258934c8a2941d59ae46f
Eduardo <eduardocasagrande71@gmail.com>
2025-06-03 20:52:41 -0300

    Feat: Many-To-Many association with joinTable

--- src/main/java/com/du/javaCourse/config/TestConfig.java
+++ src/main/java/com/du/javaCourse/config/TestConfig.java
@@ -38,24 +38,31 @@ public class TestConfig implements CommandLineRunner {
 
         User u1 = new User(null, "Teste01", "teste1@gmail.com", "99999999", "123456");
         User u2 = new User(null, "Teste02", "teste2@gmail.com", "88888888", "123456");
+        userRepository.saveAll(List.of(u1, u2));
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+        orderRepository.saveAll(List.of(o1, o2, o3));
 
         Category c1 = new Category(null, "Electronics");
         Category c2 = new Category(null, "Books");
         Category c3 = new Category(null, "Computers");
+        categoryRepository.saveAll(List.of(c1, c2, c3));
 
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
         Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
         Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
         Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+        productRepository.saveAll(List.of(p1, p2, p3, p4, p5));
 
-        userRepository.saveAll(List.of(u1, u2));
-        orderRepository.saveAll(List.of(o1, o2, o3));
-        categoryRepository.saveAll(List.of(c1, c2, c3));
+        p1.getCategories().add(c2);
+        p2.getCategories().add(c1);
+        p2.getCategories().add(c3);
+        p3.getCategories().add(c3);
+        p4.getCategories().add(c3);
+        p5.getCategories().add(c2);
         productRepository.saveAll(List.of(p1, p2, p3, p4, p5));
 
     }
